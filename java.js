519705860d2b5e41da39f8b8e0b4dda8fdef10cc
Linn Gadevik <gadeviklinn@gmail.com>
2023-03-01 10:06:38 +0100

    bla

---
+++ java.js
@@ -0,0 +1,73 @@
+document.querySelector('#search').addEventListener("click", getPokemon);
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function lowerCaseName(string) {
+    return string.toLowerCase();
+}
+
+function getPokemon(e) {
+  const name = document.querySelector("#pokemonName").value;
+  const pokemonName = lowerCaseName(name);
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then((response) => response.json())
+    .then((data) =>{
+       document.querySelector(".pokemonBox").innerHTML = `
+       <div>
+            <img src="${data.sprites.other["official-artwork"].front_default}"
+             alt="${capitalizeFirstLetter(data.name)}">
+        </div>
+        <div class="pokemonInfo">
+            <h1>${capitalizeFirstLetter(data.name)}</h1>
+            <p>weight: ${data.weight}</p>
+        </div>
+       `;
+    })
+    .catch((err)=> {
+        console.log("Pokemon not found", err);
+    });
+
+    e.preventDefault();
+}
+let names = [];
+let sortedNames = names.sort();
+
+
+let input = document.getElementById("pokemonName");
+
+pokemonName.addEventListener("keyup", (e) => {
+    removeElements();
+   for (let i of sortedNames) {
+    
+    if (
+        i.toLowerCase().startsWith(pokemonName.value.toLowerCase()
+        ) &&
+          input.value !=""
+          ); {
+        let listItem = document.createElement("li");
+
+        listItem.classList.add("list-items");
+        listItem.style.cursor = "pointer";
+        listItem.setAttribute("onclick", "displayNames('" + i + "')");
+
+        let word = "<b>" + i.substr(0,input.value.length) + "</b>";
+        word += i.substr(input.value.length);
+        listItem.innerHTML = word;
+        document.querySelector(".list").appendChild(listItem);
+    }
+   }
+});
+
+function displayNames(value) {
+    input.value = value;
+    removeElements();
+}
+function removeElements() {
+    let items = document.querySelectorAll(".list-items");
+    items.forEach((item) => {
+        item.remove();
+    });
+}
